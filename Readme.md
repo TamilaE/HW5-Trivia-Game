1341e829102667ea0351ade29ddb7650bdcbffbf
TamilaTE <tamilaaghdami@yahoo.com>
2018-10-30 13:49:27 -0700

    added readme, modified app.js

--- Readme.md
+++ Readme.md
@@ -1,2 +1,19 @@
-#Trivia Game!
-###You will have a limited time to answer to the multiple choice questions about Disney.
+#**Trivia Game! (BASIC)**
+## Overview
+###This is a Trivia game using JavaScript for the logic and jQuery to manipulate HTML.
+- By playing this Disney Trivia game test your knowelede about Disney cartoons.
+
+##How to play:
+- To play click the START button and the timer will start.
+- Then the question and multiple choices(4) will apear. 
+- You have 30 seconds to answer each question.
+- If you fail to answer the question within the 30 second the answered will apear and the question counts as UNANSWERED.
+- The way that you select the answer is really simple just hover over the choice that you want to select and then click on it. (if you notice when you hover over the choices they become pink!)
+- After your selection you immediately will see the result along side with a picture related to the question asked.
+- When you answered all the question you will see your overall result that will show you the total of: 
+1. Your corrrect answers
+2. Your incorrect answered
+3. the unanswered questions
+- After you saw your result and you became familiar with the question you can simply press the START OVER button and play again
+##### Have fun!
+
--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,162 +1,132 @@
-$(document).ready(function() {
-    
-    var timer = 50;
-    var clock;
-    var correctAns = 0;
-    var incorrectAns = 0;
-    var unanswered = 0;
-    var currentQuestion= 0;
-   
-    var gameQuestions = ["What film has a snake called Kaa in it?", "In Aladdin where does Aladdin find the Genie's lamp?", "What is the name of the boy the mice rescue in the film The Rescuers Down Under?", "What is the name of the baboon in The Lion King?"];
-    var gameAnswers = ["The Jungle Book", "The Cave of Wonders", "Cody", "Rafiki"];
-    var choiceOne = ["Toy Story", "The Cove of Diamonds","Jody", "Tafiki" ];
-    var choiceTwo = ["Alice in Wonderland", "The Cave of Hearts", "Cody","Pafiki"  ];
-    var choiceThree = ["The Jungle Book", "The Cave of Wonders", "Tody", "Safiki"];
-    var choiceFour = ["Beauty and the Beast","The Cove of Dreams", "Kody", "Rafiki"];
-    
-    function showFun() {
-        $("#questions").show();
-        $("#multipleChioceOne").show();
-        $("#multipleChioceTwo").show();
-        $("#multipleChioceThree").show();
-        $("#multipleChioceFour").show();
-    }
+var panel = $("#quiz-area");
+var countStartNumber = 30;
 
-    function hideFun() {
-        $("#questions").hide();
-        $("#multipleChioceOne").hide();
-        $("#multipleChioceTwo").hide();
-        $("#multipleChioceThree").hide();
-        $("#multipleChioceFour").hide();
-
-    }
-
-   function hideAnswers () {
-       $("#correctChoice").hide();
-       $("#incorrectChoice").hide();
-       $("#unansweredQuestions").hide();
-       $("#Replay").hide();
-   }
-
-   function inPage () {
-       hideAnswers();
-       $("#answers").hide();
-       $("#timer").show();
-       showFun();
-       $("#questions").html(gameQuestions[currentQuestion]);
-       $("#multipleChioceOne").html(choiceOne[currentQuestion]);
-       $("#multipleChioceTwo").html( choiceTwo[currentQuestion]);
-       $("#multipleChioceThree").html(choiceThree[currentQuestion]);
-       $("#multipleChioceFour").html(choiceFour[currentQuestion]);
-   }
-
-   $("#multipleChioceOne").on("click", checkAns)
-   $("#multipleChioceTwo").on("click", checkAns)
-   $("#multipleChioceThree").on("click", checkAns)
-   $("#multipleChioceFour").on("click", checkAns)
-  
-   function checkAns() {
-       hideFun();
-       
-       if ($(this).text() === gameAnswers[currentQuestion]) {
-           stoptime();
-           selectedAns = true; 
-           $("#answers").show();
-           $("#answers").html("WELL DONE!");
-           inPage();
-           correctAns++;
-           currentQuestion++;
-       }
-      else {
-          stopTime();
-          selectedAns = true;
-          $("#answers").show();
-          $("#answers").html("OH NO! THE CORRECT ANSWER IS: " + gameAnswers[currentQuestion]);
-          inPage();
-          incorrectAns++;
-          currentQuestion++;
-      }
-
-      gameEnds(); 
-
-}
-function gameEnds() {
-    
-        if (currentQuestion === gameQuestions.length) {
-            $("#timer").hide();
-            showResu();
-            currentQuestion = 0;
-            $(".play").show();
-            $(".play").on("click", function (){
-                reset();
-                playGame();
-
-              });
-        }
-}
-
-    
-
-    function timerReset() {
-        timer = 40; 
-    }
-
-    function showTimer() {
-        timer--;
-        $("#timer").html("YOU HAVE " + timer + "LEFT!");
-
-        if (timer <= 0 ) {
-            hideFun();
-            stoptime();
-            $("#answers").show();
-            $("#answers").html("No TIME LEFT! THE ANSWER IS : " + gameAnswers[currentQuestion]);
-            unanswered++;
-            currentQuestion++;
-            gameEnds();
-        }
-    }
-
-    function startTimer() {
-        clearInterval(clock);
-       clock = setInterval(showTimer, 1000);
-    }
-
-    function stopTime() {
-        clearInterval(clock);
-        timerReset();
-        if(currentQuestion < gameQuestions.length - 1) {
-            setTimeout(startTimer, 2000);
-            setTimeout(inPage, 3000);
-        }
-    }
-
-    timerReset();
-
-    function showResu() {
-        $("#correctChoice").show();
-        $("#correctChoice").html("CORRECT: " + correctAns);
-        $("#incorrectChoice").show();
-        $("#incorrectChoice").html("INCORRECT: " + incorrectAns);
-        $("#unansweredQuestion").show();
-        $("#UnansweredQuestion").html("UNANSERED: " + unanswered);
-        $("#replay").show();
-        $("#replay").html("PLAY AGAIN");
-
-    }
-    
-    function reset() {
-        correctAns = 0;
-        incorrectAns = 0;
-        unanswered = 0;
-    }
-    
-    function playGame() {
-        $(".play").hide();
-        startTimer();
-        inPage();
-    }
-
-    $(".play").on("click", function() {
-        playGame();
-    });
+$(document).on("click", "#start-over", function(e) {
+    game.reset();
 });
+
+$(document).on("click", ".answer-button", function(e) {
+    game.clicked(e)
+});
+
+$(document).on('click', '#start', function(e) {
+    $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
+    game.loadQuestion();
+});
+
+var questions = [{
+    question:"What film has a snake called Kaa in it?",
+    answers: ["The Jungle Book", "Toy Story", "Alice in Wonderland","Beauty and the Beast"],
+    correctAnswer: "The Jungle Book",
+    image: "assets/images/JungelBook.jpg"
+}, {
+    question:"In Aladdin where does Aladdin find the Genie's lamp?",
+    answers: ["The Cave of Wonders", "The Cove of Diamonds", "The Cave of Hearts", "The Cove of Dreams"],
+    correctAnswer: "The Cave of Wonders",
+    image: "assets/images/AladdinLamp.jpg"
+}, {
+    question:"What is the name of the boy the mice rescue in the film The Rescuers Down Under?",
+    answers: ["Jody", "Cody","Tony", "Kody"],
+    correctAnswer: "Cody",
+    image: "assets/images/rescuersduposter.jpg"
+},{
+    question:"What is the name of the baboon in The Lion King?",
+    answers: ["Tafiki", "Safiki","Pafiki", "Rafiki"],
+    correctAnswer: "Rafiki",
+    image: "assets/images/simba-rafiki.jpg"
+}];
+
+var game = {
+    questions:questions,
+    currentQuestion:0,
+    counter:countStartNumber,
+    correct:0,
+    incorrect:0,
+    countdown: function(){
+        game.counter--;
+        $('#counter-number').html(game.counter);
+
+        if(game.counter === 0){
+            console.log("TIME UP");
+            game.timeUp();
+        }
+    },
+    loadQuestion: function(){
+        timer = setInterval(game.countdown, 1000);
+        panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+        for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+          panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+        }
+    },
+    nextQuestion: function(){
+        game.counter = countStartNumber;
+        $('#counter-number').html(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    },
+    timeUp: function (){
+        clearInterval(timer);
+        $('#counter-number').html(game.counter);
+    
+        panel.html('<h2>Out of Time!</h2>');
+        panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+        panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+    
+        if (game.currentQuestion === questions.length - 1){
+          setTimeout(game.results, 3 * 1000);
+        } else {
+          setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+    results: function() {
+        clearInterval(timer);
+    
+        panel.html('<h2>All done, heres how you did!</h2>');
+        $('#counter-number').html(game.counter);
+        panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+        panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+        panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+        panel.append('<br><button id="start-over">Start Over?</button>');
+    },
+    clicked: function(e) {
+        clearInterval(timer);
+    
+        if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+          this.answeredCorrectly();
+        } else {
+          this.answeredIncorrectly();
+        }
+    },
+    answeredIncorrectly: function() {
+        game.incorrect++;
+        clearInterval(timer);
+        panel.html('<h2>Nope!</h2>');
+        panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+    
+        if (game.currentQuestion === questions.length - 1){
+          setTimeout(game.results, 3 * 1000);
+        } else {
+          setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+    answeredCorrectly: function(){
+        clearInterval(timer);
+        game.correct++;
+        panel.html('<h2>Correct!</h2>');
+        panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+    
+        if (game.currentQuestion === questions.length - 1){
+          setTimeout(game.results, 3 * 1000);
+        } else {
+          setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+    reset: function(){
+        this.currentQuestion = 0;
+        this.counter = countStartNumber;
+        this.correct = 0;
+        this.incorrect = 0;
+        this.loadQuestion();
+    }
+};
